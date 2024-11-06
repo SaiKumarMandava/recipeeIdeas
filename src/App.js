dfbee4de6560c22b0115cd0d065bbaee8b19912e
@sai133 <sai@cloudexpresssolutions.com>
2024-11-06 18:46:21 +0530

    scroll to top added

--- src/App.js
+++ src/App.js
@@ -10,12 +10,23 @@ import IndividualRecipess from './components/individualRecipess';
 import Healthy from './components/healthy';
 import Comfort from './components/comfort';
 import Adventurous from './components/adventurous';
+import ScrollToTop from "react-scroll-to-top";
+
 function App() {
 
   return (
     <div>
       <NavBar/>
      {/* <Categories/> */}
+     <ScrollToTop smooth color="#6f00ff" 
+     className='animate-bounce'
+     style={{
+      display:"flex",
+      justifyContent:"center",
+      alignItems:"center",
+  padding:"10px 10px 10px 10px"
+     }}
+     />
      <Routes>
      <Route 
        exact path='/'
